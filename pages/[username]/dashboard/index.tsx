6874f000b943e3680c809eb02099a9f19832c3ba
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-03-07 15:20:00 +0200

    FEAT: user dashboard and settings routes

---
+++ pages/[username]/dashboard/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Dashboard: NextPage = () => {
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">dashboard page</h1>
+        </>
+    )
+}
+
+export default Dashboard
