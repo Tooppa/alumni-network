b240c252d4159b69b78c5496ec22ae40ede81e99
arttuhar <arttuhar@gmail.com>
2022-03-08 13:48:19 +0200

    FEAT: Add footer mockup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Footer from '../components/footer/Footer'
 import Navbar from '../components/navbar/Navbar'
 import styles from '../styles/Home.module.css'
 
@@ -69,6 +70,7 @@ const Home: NextPage = () => {
           </span>
         </a>
       </footer>
+      <Footer />
     </div>
   )
 }
