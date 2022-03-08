12552257aaf2e0904c794f5600554f4d243510f8
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-03-08 12:26:40 +0200

    FEAT: add detail functionality to dahsboard and mock data

--- pages/[username]/dashboard/index.tsx
+++ pages/[username]/dashboard/index.tsx
@@ -1,9 +1,24 @@
 import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import Details from '../detail'
 
 const Dashboard: NextPage = () => {
+    const router = useRouter()
+    const { username } = router.query
+    //this should find the corresponding user and check if the token matches
+    //if there is a corresponding user but token does not match show just detail
+    //if both match show buttons for settings and dashboard
+    const fakeUser = {
+        name: "Sleepper",
+        profilePic: "/srctest",
+        workStatus: "Sleeping",
+        bio: "Feeling tired",
+        funFact: "Likes to sleep"
+    }
+    
     return (
         <>
-            <h1 className="text-3xl font-bold underline">dashboard page</h1>
+            <Details user={fakeUser}/>
         </>
     )
 }
