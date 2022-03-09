2674b43c99cbd2773a5035f54a665cf5df065422
arttuhar <arttuhar@gmail.com>
2022-03-09 10:19:44 +0200

    REFACTOR: Remove unnecessary imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import Footer from '../components/footer/Footer'
-import Navbar from '../components/navbar/Navbar'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
