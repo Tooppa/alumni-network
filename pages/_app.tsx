966c3853ce3bb60efd67549283b17dd6bda02131
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-03-09 15:23:48 +0200

    CHORE: keycloak.json added containing the auth service info

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Layout from '../components/layout/Layout'
+import Layout from '../Components/layout/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
