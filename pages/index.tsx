74fdf748e225bb80d9c092f1e9f5d9be882456c1
arttuhar <arttuhar@gmail.com>
2022-03-09 13:02:13 +0200

    FEAT: Add create post component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import CreatePost from './timeline/CreatePost'
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +14,8 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet"></link>
       </Head>
+
+      <CreatePost />
 
       <main className={styles.main}>
         <h1 className={styles.title}>
