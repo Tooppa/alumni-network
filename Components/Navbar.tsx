c02509aa1c72947e5bc8a4a5e6f40ed5dd8e78de
arttuhar <arttuhar@gmail.com>
2022-03-11 12:30:44 +0200

    FEAT: Add sticky navbar, fixes for navbar and footer styling, change colors

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -3,15 +3,15 @@ import React from 'react';
 
 const Navbar = () => {
     return(
-        <div className="bg-gray-800 px-4 py-6">
+        <div className="bg-gray-800 px-4 py-6 shadow-md sticky top-0 z-50">
             <div className="container flex flex-wrap justify-between items-center mx-auto">
                 <Link href="/">
                     <a className="flex items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" className='fill-current text-blue-300 mr-3 h-8'>
-                            <path d="M512 32H120c-13.25 0-24 10.75-24 24L96.01 288c0 53 43 96 96 96h192C437 384 480 341 480 288h32c70.63 0 128-57.38 128-128S582.6 32 512 32zM512 224h-32V96h32c35.25 0 64 28.75 64 64S547.3 224 512 224zM560 416h-544C7.164 416 0 423.2 0 432C0 458.5 21.49 480 48 480h480c26.51 0 48-21.49 48-48C576 423.2 568.8 416 560 416z" />
-                        </svg>
-                        <span className="text-white self-center text-xl whitespace-nowrap">
-                            Alumni Network
+                        <span className="text-white self-center text-xl whitespace-nowrap font-thin">
+                            alumni
+                        </span>
+                        <span className="text-green-300 self center text-xl whitespace-nowrap">
+                            Network
                         </span>
                     </a>
                 </Link>
