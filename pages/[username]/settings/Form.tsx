f6458d8502f5100dd4ad44f10d18ae4617e7fa8f
arttuhar <arttuhar@gmail.com>
2022-03-14 14:01:11 +0200

    REFACTOR: Modify settings form component styles

--- pages/[username]/settings/Form.tsx
+++ pages/[username]/settings/Form.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import { FormEvent } from 'react'
 
 const SettingsForm = ({ data }: any) => {
@@ -19,7 +20,33 @@ const SettingsForm = ({ data }: any) => {
         // result.user => 'Ada Lovelace'*/
     }
     return (
-        <div className="flex w-full justify-center">
+        <div className="bg-white my-6 p-4 rounded-sm shadow-lg">
+            <form onSubmit={registerUser} className="grid grid-cols-4">
+                <div className="col-span-1 p-6">
+                    <div className="rounded-full ring-2 ring-green-400 flex">
+                        <Image src="/vercel.svg" alt="Profile pic" width={200} height={200} />
+                    </div>
+                </div>
+                <div className="col-span-3 p-6">
+                    <div className="">
+                        <div className="text-gray-800 text-2xl font-base mb-6">
+                            <h1>Username</h1>
+                        </div>
+                        <div>
+                            <input type="text" className="w-full border border-gray-200 rounded-sm mb-6 px-2 py-1 text-sm text-gray-600 focus:outline-none focus:border-gray-300" placeholder="Title" />
+                            <textarea rows={4} className="border border-gray-200 w-full mb-4 px-2 py-1 text-sm text-gray-600 rounded-sm focus:outline-none focus:border-gray-300" placeholder="Post something..."></textarea>
+                            <input type="text" className="w-full border border-gray-200 rounded-sm mb-6 px-2 py-1 text-sm text-gray-600 focus:outline-none focus:border-gray-300" placeholder="Title" />
+                        </div>
+                    </div>
+                    <div className="flex">
+                        <div className="ml-auto">
+                            <button type="submit" className="text-white mr-2 bg-green-400 shadow hover:bg-green-300 rounded-sm text-sm px-3 py-2 text-center">Submit</button>
+                            <button type="reset" className="text-white bg-red-400 shadow hover:bg-red-300 rounded-sm text-sm px-3 py-2 text-center">Cancel</button>
+                        </div>
+                    </div>
+                </div>
+            </form>
+            {/*
             <form onSubmit={registerUser} className="flex flex-col w-full max-w-3xl  border border-solid border-gray-900 p-7">
                 <div className="flex justify-between m-2">
                     <label htmlFor="name">Name</label>
@@ -67,6 +94,7 @@ const SettingsForm = ({ data }: any) => {
                 </div>
                 <button type="submit">Register</button>
             </form>
+            */}
         </div>
     )
 }
