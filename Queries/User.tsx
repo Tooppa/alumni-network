8ee6aead6a9d0c8582a48a7ad566bbf79a36d24c
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-03-14 11:54:33 +0200

    FEAT: comments are now fetched per post

--- Queries/User.tsx
+++ Queries/User.tsx
@@ -1,9 +1,9 @@
 export const getUsers = () =>
-    fetch(`https://alumni-network-api.azurewebsites.net/user`).then(
+    fetch(`https://alumni-network-api.azurewebsites.net/api/v1/user`).then(
         res => res.json()
     );
 
 export const getUser = ( id: number ) =>  
-fetch(`https://alumni-network-api.azurewebsites.net/api/user/${id}`).then(
+    fetch(`https://alumni-network-api.azurewebsites.net/api/v1/user/${id}`).then(
         res => res.json()
     );
