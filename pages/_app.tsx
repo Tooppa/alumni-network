8283015edd6d815050fd09c6a9cd9752d2fc425c
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-03-14 13:41:22 +0200

    FIX: diffrent type of authentication check

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,11 +23,17 @@ function MyApp({ Component, pageProps, cookies }: AppProps & InitialProps) {
   const queryClient = new QueryClient()
   const env = process.env.NODE_ENV
   let initOptions = {}
-  if (env === "production")
+  if (env == "production")
     initOptions = {
       onLoad: 'login-required',
       checkLoginIframe: false
     }
+  else {
+    initOptions = {
+      onLoad: '',
+      checkLoginIframe: false
+    }
+  }
   return (
     <SSRKeycloakProvider
       keycloakConfig={keycloakCfg}
