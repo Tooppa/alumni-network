afb72460cacb5fdb553d5113eeae857ef7361795
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-03-15 14:00:22 +0200

    FEAT: basic logout button

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,14 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import CreatePost from './timeline/CreatePost'
 import { ParsedToken } from '../Types/Token'
+import { KeycloakInstance } from 'keycloak-js'
 
 
 const Home: NextPage = () => {
-  const { keycloak } = useKeycloak()
+  const { keycloak } = useKeycloak<KeycloakInstance>()
   const token: string | undefined = keycloak?.token
   console.log(token);
-  
+
   const welcomeMessage =
     keycloak?.authenticated
       ? "you are logged in"
@@ -29,6 +30,7 @@ const Home: NextPage = () => {
         <p>
           {welcomeMessage}
         </p>
+        <button className='border border-solid border-gray-900 p-3' onClick={() => keycloak?.logout()}>Logout</button>
       </main>
     </div>
   )
