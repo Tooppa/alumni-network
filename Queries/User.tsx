e59369e2b3ab6bbc4bf20595a91815568ae4a2b0
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-03-15 15:36:20 +0200

    FIX: added then to query

--- Queries/User.tsx
+++ Queries/User.tsx
@@ -13,4 +13,6 @@ export const getUser = ( id: number, token: string = "" ) =>
 export const login = (token: string = "") =>
     fetch('https://alumni-network-api.azurewebsites.net/api/v1/user/login', {
       headers: { Authentication: `Bearer ${token}` }
-    })
+    }).then(
+        res => res.json()
+    );
