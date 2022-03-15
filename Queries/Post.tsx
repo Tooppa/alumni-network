2ac73cd5f49e8ac9d1459f016639afa119bd2000
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-03-15 09:55:24 +0200

    FEAT:authorizaton for few endpoint calls

--- Queries/Post.tsx
+++ Queries/Post.tsx
@@ -1,9 +1,13 @@
-export const getPosts = () => 
-    fetch(`https://alumni-network-api.azurewebsites.net/api/v1/post`).then(
+export const getPosts = (token: string = "") =>
+    fetch(`https://alumni-network-api.azurewebsites.net/api/v1/post`, {
+        headers: { Authentication: `Bearer ${token}` }
+    }).then(
         res => res.json()
     );
 
-export const getPost = ( id: number ) =>  
-    fetch(`https://alumni-network-api.azurewebsites.net/api/v1/post/${id}`).then(
+export const getPost = (id: number, token: string = "") =>
+    fetch(`https://alumni-network-api.azurewebsites.net/api/v1/post/${id}`, {
+        headers: { Authentication: `Bearer ${token}` }
+    }).then(
         res => res.json()
     );
