c671f8d798b29b4579a2bbec78950c792b2a981d
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-03-16 13:34:39 +0200

    FEAT: topic fetchin now works

--- pages/topic/[topicname]/index.tsx
+++ pages/topic/[topicname]/index.tsx
@@ -1,10 +1,17 @@
+import { useKeycloak } from "@react-keycloak/ssr";
+import { KeycloakInstance } from "keycloak-js";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useQuery } from "react-query";
+import { getTopic } from "../../../Queries/Topic";
 import TopicDetails from "./TopicDetails";
 
 const Topic: NextPage = () => {
     const router = useRouter()
     const { topicname } = router.query
+    const { keycloak } = useKeycloak<KeycloakInstance>()
+    const token: string | undefined = keycloak?.token
+    const { data, status } = useQuery('topic', () => getTopic(Number(topicname), token), {enabled: !!token})
 
     const fakeTopic = {
         name: "Front end problems",
@@ -27,7 +34,7 @@ const Topic: NextPage = () => {
 
     return (
         <>
-            <TopicDetails topic={fakeTopic} />
+            <TopicDetails topic={data} />
         </>
     )
 }
