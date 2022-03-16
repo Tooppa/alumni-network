4abab8e74d85a2bed3f93e9742f513707d7a3397
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-03-16 13:23:56 +0200

    FEAT: groups are now fetched

--- pages/group/[groupname]/index.tsx
+++ pages/group/[groupname]/index.tsx
@@ -1,11 +1,19 @@
+import { useKeycloak } from "@react-keycloak/ssr";
+import { KeycloakInstance } from "keycloak-js";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useQuery } from "react-query";
+import { getGroup, getGroups } from "../../../Queries/Group";
 import GroupDetails from "./GroupDetails";
 
 const Group: NextPage = () => {
     const router = useRouter()
     const { groupname } = router.query
+    const { keycloak } = useKeycloak<KeycloakInstance>()
+    const token: string | undefined = keycloak?.token
+    const { data, status } = useQuery('group', () => getGroup(Number(groupname), token), {enabled: !!token})
 
+    
     const fakeGroup = {
         name: "Amazing group",
         description: "Both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.",
@@ -25,12 +33,13 @@ const Group: NextPage = () => {
             }
         ]
     }
-
-    return (
-        <>
-            <GroupDetails group={fakeGroup} />
-        </>
-    )
+    if (status === "success")
+        return (
+            <>
+                <GroupDetails group={data} />
+            </>
+        )
+    else return <></>
 }
 
 export default Group
