685a8cc7d19aa585d77370e0194ef41d4e2f4708
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-03-17 13:53:37 +0200

    FEAT: proper fields for the settings page

--- pages/user/[username]/settings/Form.tsx
+++ pages/user/[username]/settings/Form.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image'
 import { FormEvent } from 'react'
+import { UserType } from '../../../../Types/Data'
 
-const SettingsForm = ({ data }: any) => {
+const SettingsForm: React.FC<{user: UserType}> = ({ user }) => {
     //currently empty function but in the future submitting a edited user could be done here
     const registerUser = async (event: FormEvent) => {
         event.preventDefault()
@@ -21,29 +22,31 @@ const SettingsForm = ({ data }: any) => {
     }
     return (
         <div className="bg-white my-6 p-4 rounded-sm shadow-lg">
+                <div className="text-gray-800 text-2xl font-base text-center">
+                    <h1>Settings</h1>
+                </div>
             <form onSubmit={registerUser} className="grid grid-cols-4">
                 <div className="col-span-1 p-6">
                     <div className="rounded-full ring-2 ring-green-400 flex">
-                        <label htmlFor="image-upload" className="cursor-pointer hover:opacity-50">
-                            <Image src="/vercel.svg" alt="Profile pic" width={200} height={200} />
+                        <label htmlFor="image-upload" className="cursor-pointer hover:opacity-80">
+                            <Image src={`/api/imagefetcher?url=${encodeURIComponent(user.pictureURL)}`} alt="Profile pic" width={200} height={200} />
                         </label>
                         <input id="image-upload" type="file" className="hidden" />
                     </div>
+                    <p className="text-center pt-2 text-sm">Click the photo to upload a new one</p>
                 </div>
                 <div className="col-span-3 p-6">
                     <div className="">
-                        <div className="text-gray-800 text-2xl font-base mb-6">
-                            <h1>Username</h1>
-                        </div>
-                        <div>
-                            <input type="text" className="w-full border border-gray-200 rounded-sm mb-6 px-2 py-1 text-sm text-gray-600 focus:outline-none focus:border-gray-300" placeholder="Title" />
-                            <textarea rows={4} className="border border-gray-200 w-full mb-4 px-2 py-1 text-sm text-gray-600 rounded-sm focus:outline-none focus:border-gray-300" placeholder="Post something..."></textarea>
-                            <input type="text" className="w-full border border-gray-200 rounded-sm mb-6 px-2 py-1 text-sm text-gray-600 focus:outline-none focus:border-gray-300" placeholder="Title" />
-                        </div>
+                        <label className="text-sm p-1">Name:</label>
+                        <input value={user.name} type="text" className="w-full border border-gray-200 rounded-sm mb-3 px-2 py-1 text-sm text-gray-600 focus:outline-none focus:border-gray-300"/>
+                        <label className="text-sm p-1">Bio:</label>
+                        <textarea value={user.bio} rows={2} className="border border-gray-200 w-full mb-2 px-2 py-1 text-sm text-gray-600 rounded-sm focus:outline-none focus:border-gray-300"/>
+                        <label className="text-sm p-1">Fun fact:</label>
+                        <textarea value={user.funFact} rows={2} className="w-full border border-gray-200 rounded-sm mb-2 px-2 py-1 text-sm text-gray-600 focus:outline-none focus:border-gray-300"/>
                     </div>
                     <div className="flex">
                         <div className="ml-auto">
-                            <button type="submit" className="text-white mr-2 bg-green-400 shadow hover:bg-green-300 rounded-sm text-sm px-3 py-2 text-center">Submit</button>
+                            <button type="submit" className="text-white mr-2 bg-green-400 shadow hover:bg-green-300 rounded-sm text-sm px-3 py-2 text-center">Save</button>
                             <button type="reset" className="text-white bg-red-400 shadow hover:bg-red-300 rounded-sm text-sm px-3 py-2 text-center">Cancel</button>
                         </div>
                     </div>
--- pages/user/[username]/settings/index.tsx
+++ pages/user/[username]/settings/index.tsx
@@ -1,15 +1,30 @@
+import { useKeycloak } from '@react-keycloak/ssr'
+import { KeycloakInstance } from 'keycloak-js'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import { useQuery } from 'react-query'
+import { getUserById } from '../../../../Queries/User'
+import { UserType } from '../../../../Types/Data'
 import SettingsForm from './Form'
 
 const Settings: NextPage = () => {
     const router = useRouter()
     const { username } = router.query
-    return (
-        <>
-            <SettingsForm/>
+    const { keycloak } = useKeycloak<KeycloakInstance>()
+    const token: string | undefined = keycloak?.token
+    const { data, status } = useQuery<UserType>('user', () => getUserById(Number(username), token), {enabled: !!token})
+
+    if (status === "success") {
+        //if there is no corresponding group query prints out a string 
+        if (typeof (data) != "object")
+            return <>
+                <p>{data}</p>
+            </>
+        return <>
+            <SettingsForm user={data}/>
         </>
-    )
+    }
+    else return <></>
 }
 
 export default Settings
