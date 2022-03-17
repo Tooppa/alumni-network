9680df956834dd99e46f7977b73e69de2f263eb0
arttuhar <arttuhar@gmail.com>
2022-03-17 10:10:57 +0200

    REFACTOR: Remove logout button and welcome message from home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,25 +11,13 @@ const Home: NextPage = () => {
   const token: string | undefined = keycloak?.token
   console.log(token);
 
-  const welcomeMessage =
-    keycloak?.authenticated
-      ? "you are logged in"
-      : "Welcome visitor. Please login to continue.";
   return (
     <div className="">
-      
       <Head>
         <title>Create Next App</title>
         <meta name="description" content="Welcome to Alumni Network" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="border border-solid border-gray-900 p-3 flex flex-col justify-center text-center mt-40">
-        <p>
-          {welcomeMessage}
-        </p>
-        <button className='border border-solid border-gray-900 p-3 mt-2 bg-green-300' onClick={() => keycloak?.logout()}>Logout</button>
-      </main>
     </div>
   )
 }
