9da0736b9e0448a71248fbf840021eefbcab22d6
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-03-17 12:24:15 +0200

    FIX: fetching for the dashboard aswell

--- pages/user/[username]/dashboard/index.tsx
+++ pages/user/[username]/dashboard/index.tsx
@@ -1,22 +1,34 @@
+import { useKeycloak } from '@react-keycloak/ssr'
+import { KeycloakInstance } from 'keycloak-js'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
+import { useQuery } from 'react-query'
 import Post from '../../../../Components/Post'
+import { getUserById } from '../../../../Queries/User'
+import { UserType } from '../../../../Types/Data'
 import Details from '../Details'
 import Buttons from './DashBoardButtons'
 
 const Dashboard: NextPage = () => {
     const router = useRouter()
     const { username } = router.query
-    //this should find the corresponding user and check if the token matches
-    //if there is a corresponding user but token does not match show just detail
-    //if both match show buttons for settings and dashboard
-    
-    return (
-        <>
-            <Details />
+    const { keycloak } = useKeycloak<KeycloakInstance>()
+    const token: string | undefined = keycloak?.token
+    const { data, status } = useQuery<UserType>('user', () => getUserById(Number(username), token), {enabled: !!token})
+
+    if (status === "success") {
+        //if there is no corresponding group query prints out a string 
+        if (typeof (data) != "object")
+            return <>
+                <p>{data}</p>
+            </>
+        return <>
+            <Details user={data}/>
             <Buttons />
         </>
-    )
+
+    }
+    else return <></>
 }
 
 export default Dashboard
