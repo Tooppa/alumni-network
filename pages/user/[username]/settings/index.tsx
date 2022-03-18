aded7215a3559793d55d41e49e66e305ddf43a11
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-03-18 09:57:08 +0200

    FEAT: settings redirect if you are not the right user

--- pages/user/[username]/settings/index.tsx
+++ pages/user/[username]/settings/index.tsx
@@ -3,7 +3,7 @@ import { KeycloakInstance } from 'keycloak-js'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import { useQuery } from 'react-query'
-import { getUserById } from '../../../../Queries/User'
+import { getUser, getUserById } from '../../../../Queries/User'
 import { UserType } from '../../../../Types/Data'
 import SettingsForm from './Form'
 
@@ -13,9 +13,10 @@ const Settings: NextPage = () => {
     const { keycloak } = useKeycloak<KeycloakInstance>()
     const token: string | undefined = keycloak?.token
     const { data, status } = useQuery<UserType>('user', () => getUserById(Number(username), token), {enabled: !!token})
-
-    if (status === "success") {
-        //if there is no corresponding group query prints out a string 
+    const { data: currentUser, status: currentStatus } = useQuery<UserType>('currentuser', () => getUser(token), {enabled: !!token})
+    
+    if (status === "success" && currentStatus === "success") {
+        if(data.id != currentUser.id) router.push(`/user/${username}`)
         if (typeof (data) != "object")
             return <>
                 <p>{data}</p>
