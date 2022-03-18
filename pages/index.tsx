aef8e8ebd91fe885553c84211bb32871d3dada35
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-03-18 10:18:49 +0200

    FEAT: added token page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,10 @@
 import { useKeycloak } from '@react-keycloak/ssr'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import CreatePost from './timeline/CreatePost'
-import { ParsedToken } from '../Types/Token'
 import { KeycloakInstance } from 'keycloak-js'
 
 
 const Home: NextPage = () => {
-  const { keycloak } = useKeycloak<KeycloakInstance>()
-  const token: string | undefined = keycloak?.token
-  console.log(token);
-
   return (
     <div className="">
       <Head>
