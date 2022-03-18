b302cae3e288659dc1537537e7d4d5063c4e9046
arttuhar <arttuhar@gmail.com>
2022-03-18 08:57:13 +0200

    FEAT: Use card grid in groups page

--- pages/group/index.tsx
+++ pages/group/index.tsx
@@ -1,10 +1,36 @@
 import type { NextPage } from "next";
+import React from "react";
+import CardGrid from "../../Components/CardGrid";
+import GroupPreview from "../GroupPreview";
 import CreateGroup from "./CreateGroup";
 
-const Groups: NextPage = () => {
+type GroupCardProps = {
+    children: React.ReactNode
+}
+
+const Groups: NextPage<GroupCardProps> = ({ children }) => {
+    const fakeGroupPreview = {
+        name: "Amazing group",
+        users: [
+            {
+                name: "Batman"
+            }
+        ],
+        posts: [
+            {
+                title: "This is test post"
+            }
+        ]
+    }
+
     return (
         <>
             <CreateGroup />
+            <CardGrid>
+                <GroupPreview groupPreview={fakeGroupPreview} />
+                <GroupPreview groupPreview={fakeGroupPreview} />
+                <GroupPreview groupPreview={fakeGroupPreview} />
+            </CardGrid>
         </>
     )
 }
