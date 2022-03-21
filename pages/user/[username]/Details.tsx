a372a1152633280aebbf65f8598f98ee860dc5b1
arttuhar <arttuhar@gmail.com>
2022-03-21 15:45:57 +0200

    FIX: Fix profile image overflow

--- pages/user/[username]/Details.tsx
+++ pages/user/[username]/Details.tsx
@@ -9,10 +9,10 @@ const Details: React.FC<{user: UserType}> = ({user}) => {
             <div className="grid grid-cols-4">
                 <div className="col-span-1 p-6">
                     <div className="rounded-full ring-2 ring-green-400 flex">
-                        <Image src={`/api/imagefetcher?url=${encodeURIComponent(user.pictureURL)}`} alt="Profile pic" width={200} height={200} />
+                        <Image src={`/api/imagefetcher?url=${encodeURIComponent(user.pictureURL)}`} alt="Profile pic" className="rounded-full" width={200} height={200} />
                     </div>
                     <div className="flex justify-center mt-4">
-                        <button type="button" className="text-white bg-green-400 shadow hover:bg-green-300 rounded-sm text-sm px-5 py-1 text-center">
+                        <button type="button" className="text-white bg-green-400 shadow hover:bg-green-300 rounded-full text-sm px-5 py-1 text-center">
                             <Link href="/settings">
                                 Edit profile
                             </Link>
@@ -20,16 +20,16 @@ const Details: React.FC<{user: UserType}> = ({user}) => {
                     </div>
                 </div>
                 <div className="col-span-3 p-6">
-                    <div className="mb-6">
+                    <div className="mb-4">
                         <h1 className="text-2xl font-base text-gray-800">{user.name}</h1>
                         <h3 className="text-md text-gray-500 font-light">{user.status}</h3>
                     </div>
-                    <div className="text-gray-800 text-sm mb-6">
-                        <p className="font-bold text-md mb-2">Bio:</p>
+                    <div className="text-gray-800 text-sm mb-4">
+                        <p className="font-bold text-md mb-1">Bio:</p>
                         <p>{user.bio}</p>
                     </div>
                     <div className="text-gray-800 text-sm">
-                        <p className="font-bold text-md mb-2">Fun fact:</p>
+                        <p className="font-bold text-md mb-1">Fun fact:</p>
                         <p>{user.funFact}</p>
                     </div>
                     
