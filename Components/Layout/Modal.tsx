c2c925152a83d25386e19d3584ed864bdd415078
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-03-21 16:50:44 +0200

    FEAT: Modal should update with infinite scroll

--- Components/Layout/Modal.tsx
+++ Components/Layout/Modal.tsx
@@ -6,6 +6,7 @@ export default function CustomModal({...props}){
     const router = useRouter()
     return (
         <Modal 
+            id="modal"
             isOpen={!!props.id} 
             onAfterClose={() => {
                 document.body.style.overflowY = "scroll" 
