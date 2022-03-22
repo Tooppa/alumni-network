d3d686a108c7aeb4f93fa62791b645430014a257
arttuhar <arttuhar@gmail.com>
2022-03-22 10:46:46 +0200

    FEAT: Add delete button to comments

--- Components/Post.tsx
+++ Components/Post.tsx
@@ -62,7 +62,7 @@ const Post: React.FC<{ post: PostType }> = ({ post }) => {
                     {commentsVisible &&
                         <div className="mt-6">
                             <hr className="border-gray-300" />
-                            <div className="ml-2 my-6">
+                            <div className="my-6">
                                 {post.replies.map((id: number) => <Comment key={id} id={id} />)}
                             </div>
                             {post.replies.length <= 0 &&
@@ -72,10 +72,8 @@ const Post: React.FC<{ post: PostType }> = ({ post }) => {
                             }
                             <div className="flex">
                                 <input type="text" placeholder="Comment..." className="border border-gray-200 w-full rounded-sm px-2 py-1 text-gray-600 text-sm focus:outline-none focus:border-gray-300" />
-                                <button type="submit" className="button text-white bg-green-400 rounded-full shadow text-center inline-flex items-center p-2 ml-2 hover:bg-green-300">
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="fill-current text-white h-4">
-                                        <path d="M511.6 36.86l-64 415.1c-1.5 9.734-7.375 18.22-15.97 23.05c-4.844 2.719-10.27 4.097-15.68 4.097c-4.188 0-8.319-.8154-12.29-2.472l-122.6-51.1l-50.86 76.29C226.3 508.5 219.8 512 212.8 512C201.3 512 192 502.7 192 491.2v-96.18c0-7.115 2.372-14.03 6.742-19.64L416 96l-293.7 264.3L19.69 317.5C8.438 312.8 .8125 302.2 .0625 289.1s5.469-23.72 16.06-29.77l448-255.1c10.69-6.109 23.88-5.547 34 1.406S513.5 24.72 511.6 36.86z" />
-                                    </svg>
+                                <button type="submit" className="text-white text-sm bg-green-400 rounded-full shadow text-center inline-flex items-center px-5 py-1 ml-2 hover:bg-green-300">
+                                    Post
                                 </button>
                             </div>
                         </div>
