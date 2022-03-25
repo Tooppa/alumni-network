32149383155cdc6e789d4621477c971d791a2686
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-03-25 14:55:15 +0200

    FIX: refresh after token is invalid. and fixed preview component placeholder scaling

--- Components/LoginManager.tsx
+++ Components/LoginManager.tsx
@@ -1,12 +1,18 @@
 import { useKeycloak } from "@react-keycloak/ssr"
 import { KeycloakInstance } from "keycloak-js"
+import { useRouter } from "next/router"
 import { useQuery } from "react-query"
 import { login } from "../Queries/User"
 
 const LoginManager = () =>{
     const { keycloak } = useKeycloak<KeycloakInstance>()
     const token: string | undefined = keycloak?.token
-    useQuery('login', () => login(token), { enabled: !!token })
+    const router = useRouter()
+    const {status} = useQuery('login', () => login(token), { 
+        enabled: !!token, 
+        onError: () => router.reload()
+    })
+
     return <></>
 }
 
